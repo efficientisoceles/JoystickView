9a98552c4f086ed90d606a0b320315764cf80ca0
Daniel Su <goldenember@gmail.com>
2016-08-09 19:08:51 -0400

    First Commit

---
+++ app/src/main/java/com/ravensoft/daniel/joysticktest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.ravensoft.daniel.joysticktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity implements JoystickView.JoystickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        JoystickView joystick = new JoystickView(this);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onJoystickMoved(float xPercent, float yPercent, int id) {
+        switch (id)
+        {
+            case R.id.joystickRight:
+                Log.d("Right Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+                break;
+            case R.id.joystickLeft:
+                Log.d("Left Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+                break;
+        }
+    }
+}
